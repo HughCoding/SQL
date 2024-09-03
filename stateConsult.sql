e3598aa5518f1b62afffa34a0ecd2d71ea9f9287
HughCoding <160680383+HughCoding@users.noreply.github.com>
2024-09-03 13:30:11 -0300

    Delete stateConsult.sql

--- stateConsult.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM estados
-
-SELECT sigla, nome AS 'Nome do Estado' FROM estados
-WHERE regiao = 'Norte'
-
-SELECT nome, regiao FROM estados
-WHERE populacao >= 2
-ORDER BY populacao DESC
